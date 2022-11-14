427e320d4ab9d5c4f3981d57fd381c389a95f649
parradam <69434868+parradam@users.noreply.github.com>
2022-11-14 20:20:35 +0000

    completed

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Coding_Tracker;
+using Coding_Tracker.models;
+using System;
+using System.Globalization;
+
+Database db = new();
+db.Initialize();
+
+var controller = new Controller();
+var validation = new Validation();
+
+var view = new View(controller, validation);
+view.Start();
